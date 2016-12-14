aa60879e87e8b2b69839e3135081b96270824a9f
Kehinde Emmanuel <emmakoko1996@gmail.com>
2016-12-14 02:56:20 +0100

    Add jquery initialisation function

--- js/app.js
+++ js/app.js
@@ -1,24 +1,27 @@
+$(function () {
 
-$('.nav li').click(function(){
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
-});
+    $('.nav li').click(function(){
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+    });
 
-new WOW().init();
+    new WOW().init();
 
-$('.carousel').carousel({
-    interval: 4000,
-    pause: "hover"
-});
+    $('.carousel').carousel({
+        interval: 4000,
+        pause: "hover"
+    });
 
 // Tooltips Initialization
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-});
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
 
-$('#collapseEx2 a').smoothScroll({
-    offset: -55,
-    speed: 600
+    $('#collapseEx2 a').smoothScroll({
+        offset: -55,
+        speed: 600
+    });
+
 });
 
 var contactApp=angular.module('contactApp',[]);
